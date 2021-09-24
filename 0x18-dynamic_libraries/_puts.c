c75fd019a98c16a11dc5253ad421a70913ddf920
lahincapie <alejandra612@hotmail.com>
2021-09-24 16:14:22 -0500

    hola

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point.
+ *@str: the variable
+ *
+ */
+void _puts(char *str)
+{
+	int msg;
+
+	for (msg = 0; *(str + msg) != '\0'; msg++)
+	{
+		_putchar(str[msg]);
+	}
+	_putchar('\n');
+}
